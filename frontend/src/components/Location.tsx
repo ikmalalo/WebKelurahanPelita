1fc133bf7431ae6f4a0c9c9e5f98276c16481d91
mpasd4.py <ikmalaliashari@gmail.com>
2026-04-13 13:46:47 +0800

    ganti lokasi email no hp

--- frontend/src/components/Location.tsx
+++ frontend/src/components/Location.tsx
@@ -18,7 +18,7 @@ export function Location() {
                   <path d="M16 21V5a2 2 0 00-2-2h-4a2 2 0 00-2 2v16"/>
                 </svg>
               </div>
-              <span>Jl. Pelita No. 123, Kelurahan Pelita, Samarinda Kota, Kalimantan Timur</span>
+              <span>Jl. Kyai H. Usman Ibrahim No.72, Pelita, Kec. Sungai Pinang, Kota Samarinda, Kalimantan Timur 75117</span>
             </div>
 
             <div className="contact-item">
@@ -27,7 +27,7 @@ export function Location() {
                   <path d="M22 16.92v3a2 2 0 01-2.18 2 19.79 19.79 0 01-8.63-3.07A19.5 19.5 0 013.07 9.82a19.79 19.79 0 01-3.07-8.68A2 2 0 012 1.19h3a2 2 0 012 1.72 12.84 12.84 0 00.7 2.81 2 2 0 01-.45 2.11L6.09 8.95a16 16 0 006 6l1.12-1.12a2 2 0 012.11-.45 12.84 12.84 0 002.81.7A2 2 0 0122 16.92z"/>
                 </svg>
               </div>
-              <span>(0541) 741234 / 0812-3456-7890</span>
+              <span>082154932966</span>
             </div>
 
             <div className="contact-item">
@@ -37,7 +37,7 @@ export function Location() {
                   <polyline points="22,6 12,13 2,6"/>
                 </svg>
               </div>
-              <span>pelayanan@kelurahanpelita.go.id</span>
+              <span>kelurahanpelita007@gmail.com</span>
             </div>
           </div>
         </div>
